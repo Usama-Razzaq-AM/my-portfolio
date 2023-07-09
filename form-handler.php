e0d1ce38d12ace953891199b5ed6dfee407f5e03
Usama Razzaq <usamarazzaq9014@gmail.com>
2023-07-09 20:12:45 +0500

    Uploaded files....

---
+++ form-handler.php
@@ -0,0 +1,28 @@
+<?php
+// Fetch form data
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Email information
+$to = 'portfolio@iamusama.online'; // Replace with your email address
+$subject = 'New Message from Portfolio Contact Form';
+$headers = "From: $name <$email>" . "\r\n";
+
+// Compose the email body
+$email_body = "Name: $name\n";
+$email_body .= "Email: $email\n";
+$email_body .= "Message:\n$message\n";
+
+// Send the email
+$mail_success = mail($to, $subject, $email_body, $headers);
+
+// Check if the email was sent successfully
+if ($mail_success) {
+    // Email sent successfully
+    header("Location: thank-you.html"); // Replace with the thank-you page URL
+} else {
+    // Error sending email
+    header("Location: error.html"); // Replace with the error page URL
+}
+?>
